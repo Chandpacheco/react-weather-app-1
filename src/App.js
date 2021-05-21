50083e3b9e32ec549fbc5ffa50772c1afdf0970e
Chandpacheco <74831984+Chandpacheco@users.noreply.github.com>
2021-05-20 21:50:14 -0400

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by
+        <a
+          href="https://www.linkedin.com/in/chandlerpacheco"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Chandler Pacheco
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Chandpacheco/react-weather-app-1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
