57ee258ae32bead85923b25eddf39013714f5e75
Chandpacheco <74831984+Chandpacheco@users.noreply.github.com>
2021-05-23 21:01:35 -0400

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by
-        <a
-          href="https://www.linkedin.com/in/chandlerpacheco"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Chandler Pacheco
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Chandpacheco/react-weather-app-1"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by
+          <a
+            href="https://www.linkedin.com/in/chandlerpacheco"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Chandler Pacheco
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Chandpacheco/react-weather-app-1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
